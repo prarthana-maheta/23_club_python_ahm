eaeabbf3a7ac01b5ea7371e1bf0669a9f5b8be86
prarthana.m <prarthana.m@simformsolutions.com>
2023-12-22 21:08:02 +0530

    latest code

---
+++ dec_20.py
@@ -0,0 +1,36 @@
+print("Hello world")
+
+a = 1
+print(a)
+
+# a=1
+# a=2
+# print(a)
+
+# a=1;b=2;c=3
+
+a,b=1,2,
+print(a,b)
+
+a,b=b,a
+print(a,b)
+
+'''
+    tgcudhcdb
+    jchdcbhv
+    chdchdb
+    bhvhvbj
+     dbjf
+'''
+
+# task 1:
+# create 2 variables a and b
+# create a calculator that will do sum of a and b,
+# subtraction of a and b, div of a and b and multiply of a and b
+
+# print("hello world \n hello world")
+# print("hello world \t hello world")
+# print("hello world\bhello world\b ")
+print("I love India\b\b\b\b\b ")
+
+print("C:\\new\game\Desktop")
---
+++ dec_22.py
@@ -0,0 +1,67 @@
+a = 1
+# type casting
+print(type(a))
+
+# type converting
+b=float(a)
+print(b)
+
+# str()
+# float()
+
+# a='1'
+# b='2'
+# print(a-b)
+
+# take input from user
+# no1 = int(input("Enter your no1:"))
+# no2 = int(input("Enter your no2:"))
+#
+# print(no1+no2)
+
+
+# a,b
+#
+# addition of a & b =
+
+# https://github.com/prarthana-maheta/23_club_python_ahm
+
+# a=int(input("Enter a:"))
+# b=int(input("Enter b:"))
+# c=int(input("Enter c:"))
+# d=int(input("Enter d:"))
+# e=int(input("Enter e:"))
+#
+# print("Avg of",a,",",b,",",c,",",d,",",e,"is",(a+b+c+d+e)/5)
+# f=1
+# print(f"avg of {a},{b},{c},{d} & {e} {f} is {a+b+c+d+e/5}")
+
+# https://www.google.com/
+#
+# first=input()
+# second=input()
+# thords=input()
+# fourth=input()
+# print(f'{first}://{second}.{thords}.{fourth}/')
+
+# data types:
+# int 7895658956589569956565656656565
+# float 1.245456564265425464265465454684
+# complex
+# string
+# list
+# tuple
+
+
+# a = 7 + 3j
+# b = 5 + 7j
+# print(type(a), type(b))
+#
+# a=1+1j
+# print(complex(a))
+# print(a)
+
+a = 7 + 3j
+b = 5 + 7j
+print(2*a * b)
+
