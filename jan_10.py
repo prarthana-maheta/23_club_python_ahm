5c10fe7a2202752f91c9ab5ab68b9fc2d1e84203
prarthana.m <prarthana.m@simformsolutions.com>
2024-01-10 20:48:58 +0530

    latest code

---
+++ jan_10.py
@@ -0,0 +1,59 @@
+abc=['AAA','aaa','aab','Aab']
+# print(min(abc))
+# print(max(abc))
+
+# methods:
+
+# # append()
+# abc[0]='xyz'
+# abc.insert(0,'zzz')
+
+# abc.append('zzz')
+# abc.extend(['1',1,2])
+# abc.extend('123')
+# print(abc)
+
+# list1=['1','abc',[1,2,3],'xyz',32]
+
+# # add 55 to list1
+# # replace 'abc' to 'aaa'
+# # write 'i' instead of 32
+# # add 78,45,32 to [1,2,3]
+
+# list1[2].extend([78,45,32])
+# list1[2].extend([78,45,])
+ 
+
+# list1=[56]
+
+# # take input from user and add power of the in the list1
+# # add 4,5,6 in the list1 before the 56
+# # add sum of 4,5,6 brfore 56
+
+# list1.insert(0,5)
+# # list1.insert(1,4)
+# list2=[4,5,6]
+# # list1[1]=list1.extend(list2)
+# # list1.insert()
+# print(list1)
+# ex output =[power,4,5,6,sum of 456,56]
+
+list1=[1,3,4,'aa',6,6,7]
+# removed=list1.pop(8)
+# print(removed)
+# print(list1)
+
+
+# list1.remove(6)
+# print(list1)
+
+
+# list1.clear()
+# print(list1)
+
+# del list1[1:5]
+# print(list1)
+ 
+# remove 2 and 3 element from list1
+# remove 'aa' from list1
+# remove 7 value from your list1 and add it to 3 index of your list1
